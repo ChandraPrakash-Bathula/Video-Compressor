49071508cf7d3531cefa9d299d898160a6ef1b56
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2026-04-28 17:45:55 -0500

    Revert ffmpeg preset back to fast for proper compression ratios

--- video_compressor.py
+++ video_compressor.py
@@ -346,7 +346,7 @@ class VideoCompressor:
                 "-crf", str(crf),
                 "-maxrate", maxrate,
                 "-bufsize", bufsize,
-                "-preset", "ultrafast",
+                "-preset", "fast",
                 "-pix_fmt", "yuv420p",
                 "-vf", vf,
             ]
